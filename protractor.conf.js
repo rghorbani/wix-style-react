d10880b6355eae3c7d7a91b086030ae63e08e305
mykas <mykolass@wix.com>
2018-08-24 16:27:53 +0300

    debugging

--- protractor.conf.js
+++ protractor.conf.js
@@ -3,6 +3,15 @@ module.exports.config = {
   baseUrl: `http://localhost:6006/`,
   troubleshoot: true,
   debug: true,
+  maxInstances: 3,
+  capabilities: {
+    browserName: 'chrome',
+
+    chromeOptions: {
+      args: ['--headless', '--disable-gpu', '--window-size=800,600']
+    },
+    maxInstances: 3
+  },
   onPrepare() {
     browser.ignoreSynchronization = true;
   }
