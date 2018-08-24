b7eb62aa04228916789ac7f0179a604bc3ce8aa0
mykas <mykolass@wix.com>
2018-08-24 15:04:30 +0300

    Trying chrome capabilities

--- protractor.conf.js
+++ protractor.conf.js
@@ -2,6 +2,12 @@ module.exports.config = {
   specs: ['test/**/*.e2e.js', 'src/**/*.e2e.js'],
   baseUrl: `http://localhost:6006/`,
 
+  capabilities: {
+    browserName: 'chrome',
+    chromeOptions: {
+      args: ['--headless', '--disable-gpu', '--window-size=800,600']
+    }
+  },
   onPrepare() {
     browser.ignoreSynchronization = true;
   }
