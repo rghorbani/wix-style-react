0f09e2b3f34a5d9dbf6f735967d3ce736d568940
mykas <mykolass@wix.com>
2018-08-24 15:54:16 +0300

    removing capabilities and adding global instances

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,14 +1,7 @@
 module.exports.config = {
   specs: ['test/**/*.e2e.js', 'src/**/*.e2e.js'],
   baseUrl: `http://localhost:6006/`,
-
-  capabilities: {
-    browserName: 'chrome',
-    chromeOptions: {
-      args: ['--headless', '--disable-gpu']
-    },
-    maxInstances: 3
-  },
+  maxInstances: 3,
   onPrepare() {
     browser.ignoreSynchronization = true;
   }
