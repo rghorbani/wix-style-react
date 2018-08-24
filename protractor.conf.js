67517bb439fff3c580fd6bb842f338e7456544b8
mykas <mykolass@wix.com>
2018-08-24 15:35:35 +0300

    removing window sizing

--- protractor.conf.js
+++ protractor.conf.js
@@ -5,7 +5,7 @@ module.exports.config = {
   capabilities: {
     browserName: 'chrome',
     chromeOptions: {
-      args: ['--headless', '--disable-gpu', '--window-size=800,600']
+      args: ['--headless', '--disable-gpu']
     }
   },
   onPrepare() {
